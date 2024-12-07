26bbba43054175875be76defe121d040a14b4c88
Naser Sami <naser_ebedo@icloud.com>
2024-12-07 13:01:59 +0300

    node chat apis

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,55 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import { json } from "body-parser";
+import http from "http";
+import { Server } from "socket.io";
 import authRoutes from "./routes/authRoutes";
 import conversationRoutes from "./routes/conversationRoutes";
+import messageRoutes from "./routes/messageRoutes";
+import { saveMessage } from "./controllers/messagesController";
 
 const app = express();
-app.use(json());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    // origin: "http://localhost:3000",
+    origin: "*",
+  },
+});
 
+app.use(json());
 app.use("/auth", authRoutes);
 app.use("/conversations", conversationRoutes);
+app.use("/messages", messageRoutes);
+
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
+
+  socket.on("joinConversation", (conversationId) => {
+    socket.join(conversationId);
+    console.log(`User joined conversation ${conversationId}`);
+  });
+
+  socket.on("sendMessage", async (message) => {
+    const { conversationId, senderId, content } = message;
+
+    console.log("Received message:", message);
+
+    try {
+      const savedMessage = await saveMessage(conversationId, senderId, content);
+      console.log("Saved message:", savedMessage);
+
+      io.to(conversationId).emit("newMessage", savedMessage);
+    } catch (error) {
+      console.error("Error saving message:", error);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.id);
+  });
+});
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- src/routes/conversationRoutes.ts
+++ src/routes/conversationRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/authMiddleware";
-import { fetchAllConversationsByUserId } from "../controllers/conversationsControllers";
+import { fetchAllConversationsByUserId } from "../controllers/conversationsController";
 
 const router = Router();
 
